3649b55c89300042a5d8e2b9fc26195384d8036f
john-simonson <90353363+john-simonson@users.noreply.github.com>
2021-11-28 11:32:10 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["./", "./index.html", "./scripts/script.js", "./scripts/index.js", "./logos/rowdy.png", "./logos/rowdyBig.png"]);
+            return cache.addAll(["./", "./index.html", "./scripts/jquery-3.6.0.min.js", "./scripts/script.js", "./scripts/index.js", "./logos/rowdy.png", "./logos/rowdyBig.png"]);
         })
     );
     console.log("Install! complete");
