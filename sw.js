b4a63861787dabfece10468af0479bdaf28b99b5
john-simonson <90353363+john-simonson@users.noreply.github.com>
2021-11-28 11:23:00 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["./", "./index.html", "./css/", "./scripts/script.js", "./scripts/index.js", "./logos/rowdy.png", "./logos/rowdyBig.png"]);
+            return cache.addAll(["./", "./index.html", "./css/bootstrap-grid.css", "./css/bootstrap-grid.css.map", "./css/bootstrap-grid.min.css", "./css/bootstrap-grid.min.css.map", "./css/bootstrap-reboot.css", "./css/bootstrap-reboot.css.map", "./css/bootstrap-reboot.min.css", "./css/bootstrap-reboot.min.css.map", "./css/bootstrap.css", "./css/bootstrap.css.map", "./css/bootstrap.min.css", "./css/bootstrap.min.css.map", "./scripts/script.js", "./scripts/index.js", "./logos/rowdy.png", "./logos/rowdyBig.png"]);
         })
     );
     console.log("Install! complete");
