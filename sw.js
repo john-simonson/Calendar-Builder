6686956e7ae4c58aecd4f0cf42f4f4b0f2c6afa5
john-simonson <90353363+john-simonson@users.noreply.github.com>
2021-11-28 00:35:56 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,11 +7,11 @@ self.addEventListener("install", e => {
     console.log("Install! complete");
 });
 
-self.addEventListener("fetch", e => {
+/*self.addEventListener("fetch", e => {
     e.respondWith(
         caches.match(e.request).then(response => {
             return response || fetch(e.request);
         })
     );
     console.log('Intercepting fetch request for: ' + e.request.url);
-});
+});*/
