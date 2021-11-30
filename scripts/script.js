132ef93c3766864625c337ab91519fc79256a6c4
john-simonson <90353363+john-simonson@users.noreply.github.com>
2021-11-30 14:14:00 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -174,6 +174,41 @@ function createTable(){
     return (new Class(class_name, class_type, class_number, section_number, start_time, days));
 }
 
+
+function parseCalendar(){
+    var url = "https://www.uml.edu/registrar/calendars/2021-fall-undergrad.aspx";
+    $.get('https://api.allorigins.win/get?url=' + encodeURIComponent(url) + '&callback=?', function(data){
+        var tmp = document.getElementById("txt").innerHTML;
+        document.getElementById("txt").innerHTML += "inserting" + data;
+        var stack = [];
+
+        $("table > tbody > tr").each(function () {
+            stack.push([$(this).find('th').eq(0).text(), $(this).find('td').eq(0).text(), closedFlag($(this).find('td').eq(0).text()), mondayFlag($(this).find('td').eq(0).text())]);
+
+        });
+        document.getElementById("txt").innerHTML = tmp;
+        console.log(stack);
+        localStorage.setItem(calendarKey, stack)
+        console.log(stack)
+        return stack
+        
+    });
+}
+
+function closedFlag(theString){
+    if (theString.includes("university closed")) {
+        return true
+    }
+    return false
+}
+
+function mondayFlag(theString){
+    if (theString.includes("Monday Class Schedule") || theString.includes("Monday class schedule")){
+        return true
+    }
+    return false
+}
+
 function createForm(){
     $('#Tab').html(" ");
 }
