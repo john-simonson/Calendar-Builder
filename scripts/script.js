bdfba19b020a60fb4b3cc690941e15d2f6bd7fc5
john-simonson <90353363+john-simonson@users.noreply.github.com>
2021-11-30 19:44:31 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -9,11 +9,24 @@ class Class{
     }
 }
 
+var academicCalendar = [];
 var date = new Date();
 
 $(function(){
     $("#calandarForm").hide();
-    parseCalendar();
+    if(window.navigator.onLine == true){
+        parseCalendar();
+        console.log(JSON.parse(localStorage.getItem("calendarKey")).length);
+    }
+    else if(localStorage.getItem("calendarKey") != null){
+        academicCalendar = JSON.parse(localStorage.getItem("calendarKey"));
+        console.log(JSON.parse(localStorage.getItem("calendarKey")).length);
+    }
+    else{
+        while(window.navigator.onLine == false){
+            alert("Error! No Internet to build Academic Calendar please load app with an internet connection!");
+        }
+    }
     first();
 });
 
@@ -62,25 +75,41 @@ function displayForm(){
 }
 
 function displayTable(){
+    var tmpTxt;
+    let testString = date.toLocaleDateString('en', { weekday: 'long' });
     $("#calandarForm").hide();
     $('#Tab').html("");
     if($("#Tab").html() == ""){
-        $('#Tab').append('<center><p>' + date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear() + '</p></center>');
+        $('#Tab').append('<center><p>' + date.toLocaleString('default', {month:'long'}) + ' ' + date.getDate() + ', ' + date.getFullYear() + '</p></center>');
         $('#Tab').append('<button onclick="deleteClasses()" class="form-group btn btn-light col-sm-3">Delete All Classes</button>');
         $('#Tab').append('<div><center><button onclick="minusWeek()" class="form-group btn btn-light col-sm-3"> -1 week </button><button onclick="minusDay()" class="form-group btn btn-light col-sm-3"> -1 day </button><button onclick="plusDay()" class="form-group btn btn-light col-sm-3"> +1 day </button><button onclick="plusWeek()" class="form-group btn btn-light col-sm-3"> +1 week </button></center></div>');
-        if(localStorage.length != 0){
+        if(localStorage.length != 1){
             var classArray = [];
             var content = "<center><table><tr><th>Class Name</th><th>Class Type</th><th>Class Number</th><th>Section Number</th><th>Start Time</th></tr>"
             for (i = 0; i <= localStorage.length; i++){
-                    if(localStorage.key(i) != "calendarKey"){
+                if(localStorage.key(i) != "calendarKey"){
                     classArray.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
                 }
             }
-            for (var key in localStorage){
-                console.log(key)
+            console.log((JSON.parse(localStorage.getItem("calendarKey")))[2][0])
+            console.log(date.toLocaleString('default', {month:'long'}) + " " + date.getDate());
+            for(i=2; i < (JSON.parse(localStorage.getItem("calendarKey")).length); i++){
+                if((JSON.parse(localStorage.getItem("calendarKey")))[i][0] == date.toLocaleString('default', {month:'long'}) + " " + date.getDate()){
+                    tmpTxt = JSON.parse(localStorage.getItem("calendarKey"))[i][1];
+                    tmpTxt = tmpTxt.replace(/\\n/g," ");
+                    $('#Tab').append("<center><b><p id='tmpTxt'>" + tmpTxt + "</p></b></center>");
+                    console.log(tmpTxt);
+                    if((JSON.parse(localStorage.getItem("calendarKey"))[i][2]) == true){
+                        testString = "Made Up Day";
+                    }
+                    if((JSON.parse(localStorage.getItem("calendarKey"))[i][3]) == true){
+                        testString = "Monday";
+                    }
+                }
             }
-            for(i = 0; i < localStorage.length; i++){
-                if(JSON.parse(localStorage.getItem(classArray[i].name)).days.includes(date.toLocaleDateString('en', {weekday:'long'}))){
+
+            for(i = 0; i < (localStorage.length - 1); i++){
+                if(JSON.parse(localStorage.getItem(classArray[i].name)).days.includes(testString)){
                     content += "<tr>"
                     content += "<td>" + JSON.parse(localStorage.getItem(classArray[i].name)).name + "</td>";
                     content += "<td>" + JSON.parse(localStorage.getItem(classArray[i].name)).type + "</td>";
